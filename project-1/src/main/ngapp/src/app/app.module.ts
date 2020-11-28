989ae2b12f518cbc506775507b60b2893f82de33
Metsuryuu <qjroman@gmail.com>
2020-11-27 20:01:48 -0500

    Angular added

---
+++ project-1/src/main/ngapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ManagerComponent } from './manager/manager.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EmployeeComponent,
+    ManagerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
