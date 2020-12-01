dc5e45eb517bd1fade4bfee1710db4e0b84807dc
Metsuryuu <qjroman@gmail.com>
2020-11-30 19:03:18 -0500

    Starting to work on JS

---
+++ project-1/src/main/webapp/JS/get-requests.js
@@ -0,0 +1,40 @@
+//JavaScript to get "Pending" requests.
+
+//Let's use a GET request.
+fetch('http://localhost:8080/project-1/getRequests', {
+    method: "POST",
+    
+    //so the idea here is I send a JSON object which gets processed in a switch and returns the relevant requests.
+    body: JSON.stringify({
+        statusId: 1
+    }),
+
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+.then(data => {
+    if(data.ok){
+        console.log("Success")
+    }else{
+        console.log("Server Error")
+    }
+})
+.catch(error => console.log("Error"))
+.then(response => response.json())
+.then(json => {
+    let li = `<tr><th>Amount</th><th>Submitted</th><th>Description</th></tr>`;
+
+    json.array.forEach(user => {
+        //This is the idea, obviously change your variable names.
+        li += <tr>
+            <td>${user.amount}</td>
+            <td>${user.submitted}</td>
+            <td>${user.description}</td>
+        </tr>
+    });
+
+    //Display to the screen.
+    document.getElementById("pending").innerHTML = li;
+});
+
